1e0e795b1ad5c55163956d29e533684938e0f76d
Kapasko <felixmorsbach@web.de>
2016-05-11 23:25:52 +0200

    Implemented GetSC2Data Routine

--- src/test/java/com/gabmingbets/gamingbetrestserver/microservices/ProductionTestCreateAndEvaluate.java
+++ src/test/java/com/gabmingbets/gamingbetrestserver/microservices/ProductionTestCreateAndEvaluate.java
@@ -1,7 +1,7 @@
 package com.gabmingbets.gamingbetrestserver.microservices;
 
 
-public class ProductionTestMain {
+public class ProductionTestCreateAndEvaluate {
 
 	public static void main(String args[]) {
 		Create_Bet_SC2 a = new Create_Bet_SC2();
