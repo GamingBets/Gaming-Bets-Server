8a140e3da84dd2aadb7dc5bde6dfe8b1e72637c1
Kapasko <felixmorsbach@web.de>
2016-05-13 23:36:35 +0200

    Implemented AdjustScore Microservice with Unit Tests

--- src/test/java/com/gabmingbets/gamingbetrestserver/microservices/All_Tests.java
+++ src/test/java/com/gabmingbets/gamingbetrestserver/microservices/All_Tests.java
@@ -5,7 +5,13 @@ import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
-@SuiteClasses({ Test_For_Bet_Creation_Microservice_SC2.class, Test_For_Bet_Creation_Microservice_Lol.class, Test_For_Bet_Evaluation_SC2.class, Test_For_Bet_Evaluation_Lol.class})
+@SuiteClasses({ 
+	Test_For_Bet_Creation_Microservice_SC2.class, 
+	Test_For_Bet_Creation_Microservice_Lol.class, 
+	Test_For_Bet_Evaluation_SC2.class, 
+	Test_For_Bet_Evaluation_Lol.class,
+	Test_For_Adjust_Score_SC2.class
+	})
 
 public class All_Tests {
 
